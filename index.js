91305c9e39e545a23ec218dfec18f00a53614854
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-05-24 05:02:51 +0600

    manageProduct delete api updated

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ async function run() {
         const ordersCollection = client.db('computerManager').collection('orders');
         const usersCollection = client.db('computerManager').collection('users');
 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
+        };
+
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
@@ -42,11 +53,18 @@ async function run() {
             res.send(services);
         });
 
-        app.post('/products', async (req, res) => {
+        app.post('/products', verifyJWT, verifyAdmin, async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
             res.send(result);
-        })
+        });
+
+        app.delete('/products/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        });
 
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
@@ -83,21 +101,14 @@ async function run() {
             res.send({ admin: isAdmin });
         })
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await usersCollection.findOne({ email: requester });
-            if (requesterAccount.role === 'admin') {
-                const filter = { email: email };
-                const updatedDoc = {
-                    $set: { role: 'admin' }
-                };
-                const result = await usersCollection.updateOne(filter, updatedDoc);
-                res.send(result);
-            }
-            else {
-                res.status(403).send({ message: 'Forbidden Access' })
-            }
+            const filter = { email: email };
+            const updatedDoc = {
+                $set: { role: 'admin' }
+            };
+            const result = await usersCollection.updateOne(filter, updatedDoc);
+            res.send(result);
         });
 
         app.put('/user/:email', async (req, res) => {
