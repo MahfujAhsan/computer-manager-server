2ed79a5421e76cd394903e87f0876bef181e384b
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-05-24 17:15:31 +0600

    orders api updated

--- index.js
+++ index.js
@@ -94,6 +94,13 @@ async function run() {
             }
         });
 
+        app.get('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const order = await ordersCollection.findOne(query);
+            res.send(order);
+        })
+
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await usersCollection.findOne({ email: email });
